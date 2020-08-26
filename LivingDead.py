7962226cdf122c9805cc14b45beb751afdc0848b
Kingkongkingston <akronnie55@gmail.com>
2020-08-25 21:20:42 -0700

    Edits

--- LivingDead.py
+++ LivingDead.py
@@ -1,31 +1,32 @@
-response = ""
-money = 100
-prestigeLevel = 0
-
 def prestige(money):
     if prestige == 1:
         print("Nice work, you're prestige level 1! You earned 10 cash!")
         money += 10
+
 
 def prestige2(money):
     if prestige == 2:
         print("Nice work, you're prestige level 2! You earned 20 cash!")
         money += 20
 
+
 def prestige3(money):
     if prestige == 3:
         print("Nice work, you're prestige level 3! You earned 30 cash!")
         money += 30
+
 
 def prestige4(money):
     if prestige == 4:
         print("Nice work, you're prestige level 4! You earned 40 cash")
         money += 40
 
+
 def prestige5(money):
     if prestige == 5:
         print("Your prestige level has maxed out! You earned 50 cash!")
         money += 50
+
 
 def call911():
     response = input('''
@@ -85,7 +86,7 @@ Do you:
 1) Call 911
 2) Run downstairs to investigate
 3) Climb down to your car
-What is your choice? >>> ''')
+What is your choice? >> ''')
     if response == "1":
         call911()
     elif response == "2":
@@ -110,16 +111,16 @@ def pizzaShop():
     its another survivor! You drive over to him, and he thinks you're a zombie, so he yells, "Back off, stinky
     citizen!", and shoots your tire.
         "Dude, i'm just another survivor, like you!", you shout.
-    Relieved, the guy walks over to you, apoligizes for shooting your tire, and shakes hands with you.
+    Relieved, the guy walks over to you, apologizes for shooting your tire, and shakes hands with you.
         "I'm terribly sorry for shooting your tire. How can I repay you?"
         "No thanks, i'm a mechanic, so I always carry a tool kit.", you say.
         After talking for a few minutes, the guy asks you,
         "I have a gang of 3 survivors and a dog a little south of here. I can help you survive if you join me...
         do you wanna join us?"
-
         Do you:
         1) Accept his offer
         2) Decline his offer, you're going to go solo
+        What is your choice>>
     ''')
 
     if response == "1":
@@ -142,10 +143,10 @@ def policeStation():
         After talking for a few minutes, the guy asks you,
         "I have a gang of 3 survivors and a dog a little south of here. I can help you survive if you join me...
         do you wanna join us?"
-        
         Do you:
         1) Accept his offer
         2) Decline his offer, you're going to go solo
+        What is your choice>>
     ''')
 
 
@@ -153,7 +154,6 @@ def leaveGasStation():
     response = input('''
     You decide that since you filled up your car with gas, you can visit the pizza shop or the police station.
     Where do you choose to go?
-
     Do you:
     1) Drive to the pizza shop
     2) Drive to the police station
@@ -163,17 +163,18 @@ def leaveGasStation():
     elif response == "2":
         policeStation()
 
+
 def gasStation():
-    input('''
+    response = input('''
     You fill up your car with gas, and walk around. You see a small shop, so ou open the door, and you hear a
     dinging noise. You flinch, but you realize that it's the bell on the door.
     The shop is empty, but you see some snack bags on the ground, and you pick them up.
     It looks like someone has messed with the bag, because there are blood marks on the bag and it has been
     opened. You look inside, and it smells weird. You take a chip, and examine it.
-
     Do you:
     1) Eat it
     2) Leave
+    What is your choice>>
     ''')
     if response == "1":
         eatChips()
@@ -182,6 +183,7 @@ def gasStation():
     else:
         urstupid()
 
+
 def pizzaShopDeath():
     input('''
     You should have filled up your car at the gas station, because your car's engine shuts off. The zombies notice
@@ -189,9 +191,11 @@ def pizzaShopDeath():
     ''')
     again()
 
+
 def policeStationDeath():
     pizzaShopDeath()
     again()
+
 
 def car():
     response = input('''
@@ -204,7 +208,7 @@ Do you:
 1) Visit the gas station
 2) Go to the pizza shop
 3) Go to the police station
-What is your choice>>>
+What is your choice>>
 ''')
     if response == "1":
         gasStation()
@@ -222,7 +226,7 @@ You press the elevator button, but nothing works, because the zombies cut out th
 Do you:
 1) Go down via the stairs
 2) Get to the car instead
-What is your choice? >>> ''')
+What is your choice? >> ''')
     if response == "1":
         stairs()
     elif response == "2":
@@ -244,5 +248,6 @@ def again():
     else:
         print("Ok. Come back and try again soon!")
         quit()
+
 
 start()
